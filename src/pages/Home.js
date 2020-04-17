3e45cda74ab4c80dee70d9591d1dec9daa33ca92
Satria H R Harsono <satriahrh@gmail.com>
2020-04-17 20:26:48 +0700

    new game

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,65 @@
 import React from "react";
+import module from './Home.module.scss'
+import Fingerprint2 from 'fingerprintjs2'
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        numberOfPlayer: '',
+      }
+    };
+
+    this.handleFormNumberOfPlayerChange = this.handleFormNumberOfPlayerChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  formValidateNumberOfPlayer(value) {
+    if (!(2 <= value && value <= 5)) {
+      alert("minimum 2, maximum 5");
+      return false
+    }
+    return true
+  }
+
+  handleFormNumberOfPlayerChange(event) {
+    let value = event.target.value;
+    if (value) {
+      if (!this.formValidateNumberOfPlayer(value)) {
+        return
+      }
+    }
+    this.setState((prevState, _) => {
+      return {
+        ...prevState,
+        form: {
+          ...prevState.form,
+          numberOfPlayer: value
+        }
+      }
+    });
+  }
+
+  handleSubmit(event) {
+    if (!this.formValidateNumberOfPlayer(this.state.form.numberOfPlayer)) {
+      return
+    }
+    alert('A name was submitted: ' + this.state.form.numberOfPlayer);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello?</h1>
+      <div className={module.game}>
+        <form onSubmit={this.handleSubmit}>
+          <h2>Mulai permainan baru?</h2>
+          <div>
+            <label>Jumlah pemain</label>
+            <input className={module.newGameInputNumberOfPlayer} type="number" min="1" max="5" placeholder="min 2, max 5" value={this.state.form.numberOfPlayer} onChange={this.handleFormNumberOfPlayerChange} />
+          </div>
+          <input type="submit" value="Mulai!!"/>
+        </form>
       </div>
     );
   }
