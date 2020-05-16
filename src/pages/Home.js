223cea6068fdcefe3498c6e5026563a7f1ad8b57
Satria H R Harsono <satriahrh@gmail.com>
2020-05-16 19:03:54 +0700

    myGames

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,54 +2,51 @@ import React from "react";
 import module from "./Home.module.scss"
 import {Link} from "react-router-dom";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
+import gql from 'graphql-tag';
+import {useQuery} from '@apollo/react-hooks';
 
-    this.state = {
-      onGoingGames: [],
-      user: {},
-    }
-  }
-
-  componentDidMount() {
-    this.setState((prevState) => (
-      {
-        ...prevState,
-        onGoingGames: [
-          {id: 1, currentPlayerId: 3},
-          {id: 2, currentPlayerId: 1},
-          {id: 3, currentPlayerId: 2},
-          {id: 4, currentPlayerId: 5},
-        ],
-        user: {
-          id: 1,
-        }
+function Home() {
+  const {error, data} = useQuery(gql`
+    query {
+      myGames {
+        id
       }
-    ))
+    }
+  `);
+
+  if (error) {
+    console.log(error)
   }
 
-  render() {
-    let onGoingGames = null
-    if (this.state.onGoingGames.length > 0) {
-      onGoingGames = <div>
-        <h2>On Going Games</h2>
-        <ul>
+  let games = []
+  if (data) {
+    games = data.myGames
+  }
+
+  let onGoingGames = null
+  if (games.length > 0) {
+    onGoingGames = <div>
+      <h2>On Going Games</h2>
+      <ul>
         {
-          this.state.onGoingGames.map((game, id) => (
-            <li key={id}><Link to={"/game/" + game.id}>{game.id}{game.currentPlayerId === this.state.user.id ? ' --- YOUR TURN!!' : null}</Link></li>
+          games.map((game, id) => (
+            <li key={id}><Link
+              to={"/game/" + game.id}>{game.id}
+              {/*{game.currentPlayerId === this.state.user.id ? ' --- YOUR TURN!!' : null}*/}
+            </Link>
+            </li>
           ))
         }
-        </ul>
-      </div>
-    }
-    return (
-      <div className={module.home}>
-        <h1>Home</h1>
-        {onGoingGames}
-      </div>
-    );
+      </ul>
+    </div>
   }
+
+  return (
+    <div className={module.home}>
+      <h1>Home</h1>
+      {onGoingGames}
+    </div>
+  );
 }
 
 export default Home
